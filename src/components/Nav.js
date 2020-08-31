a0fa4474d9b7e955b8fd1ea9d02ea9c65de31dfe
ztcollazo <ztcollazo08@gmail.com>
2020-08-31 18:09:37 -0400

    update Auth0

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,75 +1,79 @@
 import React from "react";
 import "../style/Nav.css";
-import { useAuth0 } from "@auth0/auth0-react";
+import { withAuth0 } from "@auth0/auth0-react";
 import { Link, NavLink } from "react-router-dom";
 
-const Nav = () =>  {
-    //this.searchInput = document.querySelector("form#search");
+class Nav extends React.Component {
+    constructor(props, pages) {
+        super(props);
 
-    //this.searchResults = [];
+        this.pages = [
+            {
+                link: "/",
+                title: "Home"
+            },
+            {
+                link: "/browse",
+                title: "Browse",
+            },
+            {
+                link: "/top-books",
+                title: "Top Books"
+            },
+            {
+                link: "/forum",
+                title: "Forum"
+            },
+            {
+                link: "/review",
+                title: "Review"
+            }
+        ];
 
-    const pages = [
-        {
-            link: "/",
-            title: "Home"
-        },
-        {
-            link: "/browse",
-            title: "Browse",
-        },
-        {
-            link: "/top-books",
-            title: "Top Books"
-        },
-        {
-            link: "/forum",
-            title: "Forum"
-        },
-        {
-            link: "/review",
-            title: "Review"
-        }
-    ];
-
-    const navLinks = pages.map((page) => {
+    this.navLinks = this.pages.map((page) => { 
             return (
                 <li>
-                    <NavLink activeClassName="active" className="sideLink" to={page.link}>{ page.title }</NavLink>
-                </li>
-            );
+                    <NavLink activeClassName="active" className="sidelink" to={ page.link }>
+                        { page.title }
+                    </NavLink>
+                </li> 
+            )
         }
     );
+    }
 
-    function handleSearch() {
+    handleSearch() {
 
     }
 
-    const { loginWithRedirect } = useAuth0;
-        
-    return (
-        <header>         
-            <nav>
-                <div id="top" >
-                    <Link to="/">
-                        <img src="/logo.png" alt="The BookWyrm Logo" />
-                        <h1 id="title" >BookWyrm</h1>
-                    </Link>
-                    <div id="holder">
-                        <form action="/results" method="get">
-                            <input placeholder="Search" type="search" id="search" />
-                            <button className="fas fa-search fa-1x" type="submit" id="searchButton" onSubmit={handleSearch} />
-                        </form>
-                        <button id="login" onclick={ () => loginWithRedirect } >Login</button>
+    render() {
+        const { loginWithRedirect } = this.props.auth0;
+
+        return (
+            <header>         
+                <nav>
+                    <div id="top" >
+                        <Link to="/">
+                            <img src="/logo.png" alt="The BookWyrm Logo" />
+                            <h1 id="title" >BookWyrm</h1>
+                        </Link>
+                        <div id="holder">
+                            <form action="/results" method="get">
+                                <input placeholder="Search" type="search" id="search" />
+                                <button className="fas fa-search fa-1x" type="submit" id="searchButton" onSubmit={ this.handleSearch } />
+                            </form>
+                            <button id="login" onclick={ () => loginWithRedirect } >Login</button>
+                        </div>
                     </div>
+                </nav>
+                <div id="side" >
+                    <ul>
+                        { this.navLinks }
+                    </ul>
                 </div>
-            </nav>
-            <div id="side" >
-                <ul>
-                    {navLinks}
-                </ul>
-            </div>
-        </header>   
-    );
+            </header>
+        );
+    }
 }
 
-export default Nav;
+export default withAuth0(Nav);
