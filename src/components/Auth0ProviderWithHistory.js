683b8c3ab054d470263928241f33d7802422e54d
ztcollazo <ztcollazo08@gmail.com>
2020-09-03 17:08:57 -0400

    add dotenv configuration

--- src/components/Auth0ProviderWithHistory.js
+++ src/components/Auth0ProviderWithHistory.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
+import dotenv from "dotenv";
 
 const Auth0ProviderWithHistory = ({ children }) => {
+    dotenv.config(
+        {
+            path: "../../.env",
+            encoding: "utf-8"
+        }
+    );
+
     const domain = process.env.AUTH0_DOMAIN;
     const clientId = process.env.AUTH0_CLIENT_ID;
 
