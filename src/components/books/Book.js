b743bfc39aeed6a07565f65e10a71ef1020e0287
ztcollazo <ztcollazo08@gmail.com>
2020-10-22 20:54:41 +0000

    Add basic book info display in the book component

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { getBook } from "../../fauna";
 
-export const Book = ({author, title, isbn, genre, summary}) => {
-    return <p>{title}, {author}</p>;
+export const Book = () => {
+    let { isbn } = useParams();
+    const book = getBook({ ref: isbn });
+    let data = book;
+    console.log(data);
+    return (
+        <div>
+            <p>Title: {data.title}</p>
+            <p>Author: {data.author}</p>
+            <p>ISBN: {data.isbn}</p>
+            <p>Description: {data.description}</p>
+        </div>
+    );
 }
 
 export default Book;
