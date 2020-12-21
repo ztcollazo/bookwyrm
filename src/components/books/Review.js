a8a0ca99b0919bb15b16b99509d301e35296d7bf
ztcollazo <ztcollazo08@gmail.com>
2020-12-21 20:51:47 +0000

    Update the rating symbols.

--- src/components/books/Review.js
+++ src/components/books/Review.js
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { getBook } from "../../fauna";
+import { Rating } from "@material-ui/lab";
 
 import { 
     Card,
@@ -13,9 +14,6 @@ import {
     IconButton
 } from "@material-ui/core";
 import {
-    StarRounded,
-    StarBorderRounded,
-    StarHalfRounded,
     ThumbUpRounded,
     ThumbDownRounded
 } from "@material-ui/icons";
@@ -54,28 +52,6 @@ const useStyles = makeStyles(() => ({
     }
 }))
 
-function Stars ({rating}) {
-    const classes = useStyles();
-    var starNum = Math.floor(rating);
-    var starDecimal = rating - starNum;
-    const hasHalfStar = starDecimal >= 0.25 && starDecimal <= 0.75;
-    const starHalves = hasHalfStar ? 1 : 0;
-    if (starDecimal > 0.75) {
-        starNum += 1;
-    }
-    const numberEmptyStars = 5 - starNum - starHalves;
-    const emptyStars = numberEmptyStars > 0 ? new Array(numberEmptyStars).fill(0) : [];
-    const fullStars = starNum > 0 ? new Array(starNum).fill(0) : [];
-
-    return starNum ? (
-        <span className={classes.stars}>
-            {fullStars.map((_, i) => <StarRounded key={`FullStar-${i}`} />)} 
-            {starHalves !== 0 ? <StarHalfRounded /> : null}
-            {emptyStars.map((_, i) => <StarBorderRounded key={`NoStar-${i}`} />)}
-        </span>
-    ) : null;
-}
-
 export const ReviewCard = ({title, authors = [], rating, raters, isbn13}) => {
     const classes = useStyles();
     var author = authors.length > 1 ? authors.join(", ") : authors[0];
@@ -83,7 +59,7 @@ export const ReviewCard = ({title, authors = [], rating, raters, isbn13}) => {
     return (
         <Card className={classes.card}>
             <CardActionArea component={Link} to={`/review/${isbn13}`} className={classes.action}>
-                <CardHeader title="Reviews" subheader={title + ' by ' + author} action={<Stars rating={rating} />} />
+                <CardHeader title="Reviews" subheader={title + ' by ' + author} action={<Rating value={rating} readOnly />} />
                 <CardContent className={classes.content}>
                     {/* 3 Reviews Go Here. To do: add a <CardActionArea /> to redirect to the reviews page. */}
                     <span className={classes.info}>
@@ -101,7 +77,7 @@ const SingleReview = (props) => {
 
     return (
         <Card className={classes.review} {...props}>
-            <CardHeader title="An OK book" subheader="John Doe" action={<Stars rating={3} />} />
+            <CardHeader title="An OK book" subheader="John Doe" action={<Rating value={3} readOnly />} />
             <CardContent>
                 <Typography>Review Body</Typography>
             </CardContent>
@@ -126,6 +102,7 @@ export const ReviewPage = () => {
             body: string,
             rating: integer,
             likes: integer,
+            book: integer - the isbn
         }
     */
 
