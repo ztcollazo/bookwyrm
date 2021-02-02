40950cda41f1cd6c21e6008c8212c8f8e03bc100
Zachary Collazo <ztcollazo08@gmail.com>
2021-02-02 14:23:53 -0500

    Delete unnecessary component.

--- src/components/books/Rating.js
+++ src/components/books/Rating.js
@@ -15,8 +15,6 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
-const Span = (props) => <span {...props}>{props.children}</span>;
-
 const TopTen = () => {
     const classes = useStyles();
     const { data = [] } = useQuery(["ratings", { sortBy: 'rating' }], queryBooks);
@@ -26,7 +24,7 @@ const TopTen = () => {
             {
                 data.map((g, i) => {
                     return (
-                        <ListItem component={Span} className={classes.li} key={i + 1}>
+                        <ListItem component="span" className={classes.li} key={i + 1}>
                             <Typography gutterBotom variant="h4" className={classes.header}>{i + 1}.</Typography>
                             <BookCard {...g} pageHref={`/book/${g.isbn13 || g.isbn10 || g.isbn}`} reviewHref={`/write-review/${g.isbn13 || g.isbn10 || g.isbn}`} />
                         </ListItem>
