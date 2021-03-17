12cf8fa92e7886486f69dcaa9e140bd8095f89d3
Zachary Collazo <ztcollazo08@gmail.com>
2021-03-17 16:10:21 -0400

    Revert previous commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -214,7 +214,7 @@ const NavLinks = (props) => {
     const classes = useStyles();
     return pages.map((page) => { 
         return (
-            <ListItem onClick={props.setOpen(false)} title={page.title} button key={page.title} component={NavLink} to={page.link} exact activeClassName={ classes.linkActive }>
+            <ListItem title={page.title} button key={page.title} component={NavLink} to={page.link} exact activeClassName={ classes.linkActive }>
                 <ListItemIcon>
                     {page.icon}
                 </ListItemIcon>
@@ -248,7 +248,7 @@ const Nav = (props) => {
     const history = useHistory();
     const classes = useStyles();
     const context = React.useContext(AppContext);
-    const {open, toggleDrawer, setOpen} = props;
+    const {open, toggleDrawer} = props;
     const [searchInput, setSearchInput] = React.useState("");
     const shouldBeResponsive = useMediaQuery('(max-width:775px)');
     const shouldMakeDrawerResponsive = useMediaQuery('(max-width:600px)')
@@ -308,7 +308,6 @@ const Nav = (props) => {
                     disableDiscovery={iOS}
                     variant={!shouldMakeDrawerResponsive ? "permanent" : "temporary"}
                     open={shouldMakeDrawerResponsive && open}
-                    onClose={() => setOpen(false)}
                     className={
                         clsx(
                             classes.drawer, {
@@ -329,7 +328,7 @@ const Nav = (props) => {
                     </List>
                     <Divider />
                     <List>
-                        <NavLinks setOpen={setOpen} />
+                        <NavLinks />
                         <ListItem component="form" style={{width: '100%'}} title="Search" onSubmit={handleSubmit} id="holder">
                             <ListItemIcon margin={5} component={IconButton} onClick={toggleDrawer}>
                                 <SearchRounded />
