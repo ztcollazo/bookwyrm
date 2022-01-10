3c80a3f435997d149e2d869d529de7638ef21ed5
Zachary Collazo <ztcollazo08@gmail.com>
2022-01-10 15:27:35 -0500

    Update dropdown

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,13 +1,15 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get static_index_url
+  test 'should get index' do
+    get root_url
     assert_response :success
   end
 
-  test "should get about" do
-    get static_about_url
+  test 'should get about' do
+    get about_url
     assert_response :success
   end
 end
