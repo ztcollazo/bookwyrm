499207f1eba5e9205986645919811e91c580e419
Zachary Collazo <ztcollazo08@gmail.com>
2022-02-24 16:24:31 -0500

    Additions to the homepage.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,8 +3,13 @@
 # Static pages controller
 class StaticController < ApplicationController
   def index
-    @random = ''
-    @random = Book.find(Book.pluck(:id).sample) if Book.exists?(1) # Make sure that there are books in db
+    bin = File.binread('tmp/recommender.bin')
+    # rubocop:disable Security/MarshalLoad
+    recommender = Marshal.load(bin)
+    # rubocop:enable Security/MarshalLoad
+    @books = Book.find(recommender.top_items(count: 2).map { |i| i[:item_id] })
+  rescue StandardError
+    @books = Book.includes(:reviews).order('reviews.rating ASC').limit(2)
   end
 
   def about; end
