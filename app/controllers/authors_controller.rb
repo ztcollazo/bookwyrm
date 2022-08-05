65ea1b48d535f59006e0d0d6f4966494ecc401db
Zachary <ztcollazo08@gmail.com>
2022-08-05 15:59:57 -0400

    Cleaner layouts

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,5 +4,6 @@
 class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
+    @books = @author.books.paginate(page: params[:page], per_page: 3)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     user = User.find_by(username: params[:id])
     if user
       @user = user
+      @reviews = @user.reviews.paginate(page: params[:page], per_page: 3)
     else
       redirect_to user_path(User.find(params[:id]).username)
     end
